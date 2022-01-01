0f11b99278cab9895929b9917e784fb76993da25
Matan Weisberg <61933614+matan034@users.noreply.github.com>
2022-01-01 12:11:32 +0200

    1.1

--- src/clients/OrderClient.java
+++ src/clients/OrderClient.java
@@ -50,7 +50,7 @@ public class OrderClient extends AbstractClient {
 	public static Map<Integer,Order> OrdersInBranch=new HashMap<>();
 	public static Map<Integer,Integer> IsOrderApproved=new HashMap<>();
 
-	public static String update_msg,insert_msg,user_login_msg,account_reg_msg,w4c_status,user_import_msg,income,addDish;
+	public static String update_msg,insert_msg,user_login_msg="",account_reg_msg,w4c_status,user_import_msg,income,addDish;
 	public static ObservableList<String> w4cList=FXCollections.observableArrayList();
 	public static ArrayList<Order> ordersInBranch=new ArrayList<>(); 
 	public static Order found_order = new Order(null,null,null,null,null);
